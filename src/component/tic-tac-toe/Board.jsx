a1c7bfd6f99bc4d1e7f4954496a68de7f0e7cbcf
5b1g0006 <5b1g0006@stust.edu.tw>
2025-06-04 14:39:17 +0800

    06/04課程,做井字遊戲,還未完成

--- src/component/tic-tac-toe/Board.jsx
+++ src/component/tic-tac-toe/Board.jsx
@@ -2,24 +2,27 @@ import React from "react";
 import Square from "./Square";
 import "./tictactoe.css";
 
-function Board() {
+function Board( xIsNext, squares, onPlay) {
+  const handleClick = (i) => {
+    console.log("Square clicked:", i)
+  }
 
   return (
     <>
       <div className="board-row">
-        <Square value={0} onSquareClick={() => {}} />
-        <Square value={1} onSquareClick={() => {}} />
-        <Square value={2} onSquareClick={() => {}} />
+        <Square value={0} onSquareClick={() => handleClick(0)} />
+        <Square value={1} onSquareClick={() => handleClick(1)} />
+        <Square value={2} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={3} onSquareClick={() => {}} />
-        <Square value={4} onSquareClick={() => {}} />
-        <Square value={5} onSquareClick={() => {}} />
+        <Square value={3} onSquareClick={() => handleClick(3)} />
+        <Square value={4} onSquareClick={() => handleClick(4)} />
+        <Square value={5} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={6} onSquareClick={() => {}} />
-        <Square value={7} onSquareClick={() => {}} />
-        <Square value={8} onSquareClick={() => {}} />
+        <Square value={6} onSquareClick={() => handleClick(6)} />
+        <Square value={7} onSquareClick={() => handleClick(7)} />
+        <Square value={8} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
--- src/component/tic-tac-toe/TicTacToe.jsx
+++ src/component/tic-tac-toe/TicTacToe.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Board from './Board';
+import { useState } from 'react';
 import './tictactoe.css';
 
 function TicTacToe() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePaly(nextSquares){
+        
+    }
+
     return (
         <>
-            <h1>Tic Tac Toe</h1>
+            <h2>Tic Tac Toe</h2>
             <div className="game">
-                
                 <div className='game-board'>
                     <Board />
                 </div>
